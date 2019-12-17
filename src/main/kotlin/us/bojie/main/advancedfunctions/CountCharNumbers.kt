9bc5cbc922d0fb04bf5e1625621c9dded6d6cd1b
Bojie Jiang <jiangbojie@gmail.com>
2019-12-16 20:56:37 -0800

    Count char numbers

---
+++ src/main/kotlin/us/bojie/main/advancedfunctions/CountCharNumbers.kt
@@ -0,0 +1,15 @@
+package us.bojie.main.advancedfunctions
+
+import java.io.File
+
+fun main() {
+    File("build.gradle").readText()
+        .toCharArray()
+        .filterNot(Char::isWhitespace)
+        .groupBy { it }
+        .map {
+            it.key to it.value.size
+        }.let {
+            println(it)
+        }
+}
