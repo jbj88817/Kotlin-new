6a78e1c17a25efaefbf6bf7e9db281f7f01de15d
Bojie Jiang <jiangbojie@gmail.com>
2019-12-28 18:36:57 -0800

    Annotation and MappingStrategy

--- src/main/kotlin/us/bojie/main/anno/ModelRef.kt
+++ src/main/kotlin/us/bojie/main/anno/ModelRef.kt
@@ -1,21 +1,60 @@
 package us.bojie.main.anno
 
 import java.lang.IllegalArgumentException
+import kotlin.reflect.KClass
+import kotlin.reflect.full.findAnnotation
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.full.primaryConstructor
 
 
+@Target(AnnotationTarget.VALUE_PARAMETER)
+annotation class FieldName(val name: String)
+
+@Target(AnnotationTarget.CLASS)
+annotation class MappingStrategy(val klass: KClass<out NameStrategy>)
+
+interface NameStrategy {
+    fun mapTo(name: String): String
+}
+
+object UnderScoreToCamel : NameStrategy {
+    // html_url -> htmlUrl
+    override fun mapTo(name: String): String {
+        return name.toCharArray().fold(StringBuilder()) { acc, c ->
+            when (acc.lastOrNull()) {
+                ' ' -> acc[acc.lastIndex] = c.toUpperCase()
+                else -> acc.append(c)
+            }
+            acc
+        }.toString()
+    }
+}
+
+object CamelToUnderScore : NameStrategy {
+    override fun mapTo(name: String): String {
+        return name.toCharArray().fold(StringBuilder()) { acc, c ->
+            when {
+                c.isUpperCase() -> acc.append('_').append(c.toLowerCase())
+                else -> acc.append(c)
+            }
+            acc
+        }.toString()
+    }
+}
+
+@MappingStrategy(CamelToUnderScore::class)
 data class UserVO(
     val login: String,
+//    @FieldName("avatar_url")
     val avatarUrl: String
 )
 
 data class UserDTO(
     var id: Int,
     var login: String,
-    var avatarUrl: String,
+    var avatar_url: String,
     var url: String,
-    var htmlUrl: String
+    var html_url: String
 )
 
 fun main() {
@@ -33,7 +72,8 @@ fun main() {
     val userMap = mapOf(
         "id" to 7081069,
         "login" to "jbj88817",
-        "avatarUrl" to "https://avatars2.githubusercontent.com/u/7081069?v=4",
+        "avatar_url" to "https://avatars2.githubusercontent.com/u/7081069?v=4",
+        "html_url" to "https://github.com/jbj88817",
         "url" to "https://api.github.com/users/jbj88817"
     )
 
@@ -49,8 +89,13 @@ inline fun <reified From : Any, reified To : Any> From.mapAs(): To {
 inline fun <reified To : Any> Map<String, Any?>.mapAs(): To {
     return To::class.primaryConstructor!!.let {
         it.parameters.map { parameter ->
-            parameter to (this[parameter.name] ?: if (parameter.type.isMarkedNullable) null
-            else throw IllegalArgumentException("${parameter.name} is required but missing"))
+            parameter to (this[parameter.name]
+                ?: (parameter.findAnnotation<FieldName>()?.name?.let(this::get))
+                ?: To::class.findAnnotation<MappingStrategy>()?.klass?.objectInstance?.mapTo(parameter.name!!)?.let(
+                    this::get
+                )
+                ?: if (parameter.type.isMarkedNullable) null
+                else throw IllegalArgumentException("${parameter.name} is required but missing"))
         }.toMap()
             .let(it::callBy)
     }
